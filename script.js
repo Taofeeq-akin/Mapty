b349058f433ee9d9e91d7ef76a1fe8a56e392f67
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-09-12 23:35:29 +0100

    Display a map using leaflet and also add an event listener on the map

---
+++ script.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+// How Maps work on JavaScript
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      // console.log(latitude, longitude);
+      // console.log(`https://www.google.com/maps/@${latitude},${longitude},12z`);
+
+      const coords = [latitude, longitude];
+
+      // How to display map using a third library (leaflet)
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // How to create a marker
+      //Add event listener to the map by using map.on
+      map.on('click', function (mapEvent) {
+        // console.log(mapEvent);
+
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker({ lat, lng })
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+
+    function () {
+      alert('Could not get your location');
+    }
+  );
